ffc121561671f3e317a2893f7cb9a44b322fcd39
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-06-18 02:10:38 +0900

    build: Add dependency on dirs 5.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
 
+use std::{process::Command, path::Path};
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -13,6 +15,22 @@ struct Args {
 }
 
 fn main() {
-    let arg: Args = Args::parse();
-    println!("{}", arg.path);
+    // let arg: Args = Args::parse();
+    // println!("{}", arg.path);
+
+    let path = Path::new("/Users/bisquit/.codew/workspaces/rpget.code-workspace");
+
+    let home_dir = dirs::home_dir();
+    if let Some(home_dir) = home_dir {
+        let codew_home_dir = home_dir.join(".codew");
+        println!("{}", codew_home_dir.to_str().unwrap());
+    }
+
+    // println!("{}", homedir.unwrap().into_os_string());
+
+    // run code command
+    // Command::new("code")
+    //     .arg(path)
+    //     .spawn()
+    //     .expect("failed");
 }
